edce6fc405b2e9f0ddfd73bc4abcf530195340cd
tinamorlock <tinamorlock@gmail.com>
2022-11-24 21:11:24 -0600

    JS slideshow (from w3 schools)

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,49 @@
+// sets an alarm based on input from the user
+
+// function countdown() {
+//    var seconds = document.getElementById("seconds").ariaValueMax;
+
+//    function tick() {
+//        seconds = seconds - 1;
+//        timer.innerHTML = seconds;
+//        var time = setTimeout(tick, 1000);
+//        if (seconds == -1) {
+//            alert("Time's up!");
+//            clearTimeout(time);
+//            timer.innerHTML = "";
+//        }
+//    }
+//    tick();
+//}
+
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+
+// for slideshow project
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
